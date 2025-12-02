f600b41d845d40b1d70c8f20dc3fa44dc2d945da
Jonnycake <jonstockton1416@gmail.com>
2025-12-02 06:12:51 -0500

    Day 2
    - Complete part 1

---
+++ day2/solution.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import sys
+
+from typing import Generator
+
+
+def get_numbers(input_file: str) -> Generator[int, None, None]:
+    with open(input_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            ranges = line.split(",")
+            for r in ranges:
+                start, end = r.strip().split('-')
+                for i in range(int(start), int(end) + 1):
+                    yield i
+
+
+def part1(input_file) -> int:
+    solution: int = 0
+
+    for number in get_numbers(input_file):
+        number_str = str(number)
+
+        if len(number_str) % 2 != 0:
+            # Skip odd digit numbers
+            continue
+
+        part_length = len(number_str) // 2
+
+        if number_str[:part_length] == number_str[part_length:]:
+            solution += number
+
+    return solution
+
+def part2(input_file) -> int:
+    solution: int = 0
+
+    return solution
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'Syntax: {sys.argv[0]} <input-file>')
+        sys.exit(1)
+
+    print(f'Part 1: {part1(sys.argv[1])}')
+    print(f'Part 2: {part2(sys.argv[1])}')
